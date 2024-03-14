854738b2a5b24171aa0c6c05a60e0f3eaa218492
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2024-03-14 09:06:33 +0530

    updated

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
 import Signup from './Components/Signup'
+import About from './Components/About';
 import {  Route, Routes } from 'react-router-dom';
 import Main from './Components/Main';
 
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element = {<LandingPage/>}/>
       <Route path='/signup' element = {<Signup/>}/>
       <Route path='/main' element = {<Main/>}/>
+      <Route path='/about' element = {<About/>}/>
     </Routes>
     </>
   )
